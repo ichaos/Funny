178318b3d6ca05bf9bcc70f4ae410e16f4c79e11
Jin Chen <ciccinocj@gmail.com>
2013-12-13 19:09:25 +0800

    Add comments for unsequenced operations

--- unsequencedOperation.c
+++ unsequencedOperation.c
@@ -6,7 +6,12 @@ int main() {
 	int i = 0;
 	int r = 1 * a[i++] + 2 * a[i++] + 3 * a[i++];
 
-	printf("result is %d\n", r);
+	printf("result is %d, index is %d\n", r, i);
 
 	return 0;
 }
+
+/**
+ * Mac OS X, clang, warning generated, result r = 140, i = 3
+ * Windows, gcc, result r = 60, i = 3
+ */
